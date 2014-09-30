aea38d7b56344b43de99f65525103fbdb42a6077
xuechengwei <xuechengweiyx@163.com>
2014-09-30 17:32:53 +0800

    add open with photoshop feature

--- com.lvye.openwithphotoshop.js
+++ com.lvye.openwithphotoshop.js
@@ -1,3 +1,33 @@
-#!/usr/local/bin/node
-var exec = require('child_process').exec;
-exec("open -b 'com.adobe.photoshop'" );
+#! /usr/local/bin/node
+var exec = require('child_process').exec,
+    fs = require('fs'),
+    http = require('http'),
+    stdin = process.stdin,
+    url = require('url'),
+    path = require('path');
+
+stdin.setEncoding('utf8');
+stdin.on('readable', function() {
+    var chunk = stdin.read(),
+        str = chunk.substring(4, chunk.length),
+        json = JSON.parse(str),
+        fileUrl = json['src'],
+        DOWNLOAD_DIR = '/var/tmp/',
+        urlSearch = url.parse(fileUrl).search,
+        fileName = url.parse(fileUrl).pathname.split('/').pop() + (urlSearch == null ? '' : urlSearch),
+        filePath = DOWNLOAD_DIR + fileName,
+        file = fs.createWriteStream(filePath),
+        protocol = fileUrl.substring(0,4) == 'https' ? https : http;
+
+    protocol.get(fileUrl, function(response) {
+        response.pipe(file);
+        exec('file -b ' + filePath, function(error, stdout, stdin) {
+            var ext = stdout.split(' ')[0].toLowerCase(),
+                newPath = filePath + '.' + ext;
+            fs.rename(filePath, newPath, function () {
+                exec("open -b 'com.adobe.photoshop' " + newPath);
+            });
+        });
+    });
+});
+
